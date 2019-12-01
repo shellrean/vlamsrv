e8818980ee3650583a2938effa898d9a589f0823
Shellrean <reanyusufali@gmail.com>
2019-12-01 16:21:12 +0700

    Login changed

--- public/js/app.js
+++ public/js/app.js
@@ -56949,146 +56949,172 @@ var render = function() {
   var _c = _vm._self._c || _h
   return _c("div", { staticClass: "container-fluid" }, [
     _c("div", { staticClass: "row", staticStyle: { height: "100vh" } }, [
-      _c("div", { staticClass: "col-md-6 bg-primary" }),
+      _vm._m(0),
       _vm._v(" "),
-      _c("div", { staticClass: "col-md-6 bg-white" }, [
-        _c("div", { staticClass: "card mt-5" }, [
-          _vm._m(0),
-          _vm._v(" "),
+      _c(
+        "div",
+        { staticClass: "col-md-6 bg-white", staticStyle: { height: "100vh" } },
+        [
           _c(
             "div",
-            { staticClass: "card-body" },
+            {
+              staticClass: "mt-auto",
+              staticStyle: {
+                position: "absolute",
+                top: "30%",
+                transform: "translateY(-30%)"
+              }
+            },
             [
-              _vm.errors.invalid
-                ? _c("div", { staticClass: "alert alert-danger rounded-0" }, [
-                    _vm._v(_vm._s(_vm.errors.invalid))
-                  ])
-                : _vm._e(),
-              _vm._v(" "),
-              _c("p", { staticClass: "text-muted" }, [
-                _vm._v(
-                  "Selamat datang di aplikasi Vlam-Sys. Silahkan masukkan username dan password"
-                )
-              ]),
-              _vm._v(" "),
-              _c("div", { staticClass: "input-group mb-3" }, [
-                _c("div", { staticClass: "input-group-prepend" }, [
-                  _c(
-                    "span",
-                    { staticClass: "input-group-text rounded-0" },
-                    [_c("font-awesome-icon", { attrs: { icon: "envelope" } })],
-                    1
-                  )
-                ]),
-                _vm._v(" "),
-                _c("input", {
-                  directives: [
-                    {
-                      name: "model",
-                      rawName: "v-model",
-                      value: _vm.data.email,
-                      expression: "data.email"
-                    }
-                  ],
-                  staticClass: "form-control",
-                  class: { "is-invalid": _vm.errors.email },
-                  attrs: { type: "email", placeholder: "Email" },
-                  domProps: { value: _vm.data.email },
-                  on: {
-                    keyup: _vm.clearError,
-                    input: function($event) {
-                      if ($event.target.composing) {
-                        return
-                      }
-                      _vm.$set(_vm.data, "email", $event.target.value)
-                    }
-                  }
-                }),
-                _vm._v(" "),
-                _vm.errors.email
-                  ? _c("div", { staticClass: "invalid-feedback" }, [
-                      _vm._v(_vm._s(_vm.errors.email[0]))
-                    ])
-                  : _vm._e()
-              ]),
-              _vm._v(" "),
-              _c("div", { staticClass: "input-group mb-4" }, [
-                _c("div", { staticClass: "input-group-prepend rounded-0" }, [
-                  _c(
-                    "span",
-                    { staticClass: "input-group-text rounded-0" },
-                    [_c("font-awesome-icon", { attrs: { icon: "lock" } })],
-                    1
-                  )
-                ]),
-                _vm._v(" "),
-                _c("input", {
-                  directives: [
-                    {
-                      name: "model",
-                      rawName: "v-model",
-                      value: _vm.data.password,
-                      expression: "data.password"
-                    }
-                  ],
-                  staticClass: "form-control",
-                  class: { "is-invalid": _vm.errors.password },
-                  attrs: { type: "password", placeholder: "Password" },
-                  domProps: { value: _vm.data.password },
-                  on: {
-                    keyup: _vm.clearError,
-                    input: function($event) {
-                      if ($event.target.composing) {
-                        return
-                      }
-                      _vm.$set(_vm.data, "password", $event.target.value)
-                    }
-                  }
-                }),
-                _vm._v(" "),
-                _vm.errors.password
-                  ? _c("div", { staticClass: "invalid-feedback" }, [
-                      _vm._v(_vm._s(_vm.errors.password[0]) + " ")
-                    ])
-                  : _vm._e()
-              ]),
+              _vm._m(1),
               _vm._v(" "),
               _c(
-                "b-button",
-                {
-                  attrs: {
-                    variant: "primary",
-                    squared: "",
-                    disabled: _vm.isLoading
-                  },
-                  on: {
-                    click: function($event) {
-                      $event.preventDefault()
-                      return _vm.postLogin($event)
-                    }
-                  }
-                },
+                "div",
                 [
-                  _c("b-spinner", {
-                    directives: [
-                      {
-                        name: "show",
-                        rawName: "v-show",
-                        value: _vm.isLoading,
-                        expression: "isLoading"
+                  _vm.errors.invalid
+                    ? _c(
+                        "div",
+                        { staticClass: "alert alert-danger rounded-0" },
+                        [_vm._v(_vm._s(_vm.errors.invalid))]
+                      )
+                    : _vm._e(),
+                  _vm._v(" "),
+                  _c("p", { staticClass: "text-muted" }, [
+                    _vm._v(
+                      "Selamat datang di aplikasi Vlam-Sys. Silahkan masukkan username dan password"
+                    )
+                  ]),
+                  _vm._v(" "),
+                  _c("div", { staticClass: "input-group mb-3" }, [
+                    _c("div", { staticClass: "input-group-prepend" }, [
+                      _c(
+                        "span",
+                        { staticClass: "input-group-text rounded-0" },
+                        [
+                          _c("font-awesome-icon", {
+                            attrs: { icon: "envelope" }
+                          })
+                        ],
+                        1
+                      )
+                    ]),
+                    _vm._v(" "),
+                    _c("input", {
+                      directives: [
+                        {
+                          name: "model",
+                          rawName: "v-model",
+                          value: _vm.data.email,
+                          expression: "data.email"
+                        }
+                      ],
+                      staticClass: "form-control",
+                      class: { "is-invalid": _vm.errors.email },
+                      attrs: { type: "email", placeholder: "Email" },
+                      domProps: { value: _vm.data.email },
+                      on: {
+                        keyup: _vm.clearError,
+                        input: function($event) {
+                          if ($event.target.composing) {
+                            return
+                          }
+                          _vm.$set(_vm.data, "email", $event.target.value)
+                        }
                       }
+                    }),
+                    _vm._v(" "),
+                    _vm.errors.email
+                      ? _c("div", { staticClass: "invalid-feedback" }, [
+                          _vm._v(_vm._s(_vm.errors.email[0]))
+                        ])
+                      : _vm._e()
+                  ]),
+                  _vm._v(" "),
+                  _c("div", { staticClass: "input-group mb-4" }, [
+                    _c(
+                      "div",
+                      { staticClass: "input-group-prepend rounded-0" },
+                      [
+                        _c(
+                          "span",
+                          { staticClass: "input-group-text rounded-0" },
+                          [
+                            _c("font-awesome-icon", { attrs: { icon: "lock" } })
+                          ],
+                          1
+                        )
+                      ]
+                    ),
+                    _vm._v(" "),
+                    _c("input", {
+                      directives: [
+                        {
+                          name: "model",
+                          rawName: "v-model",
+                          value: _vm.data.password,
+                          expression: "data.password"
+                        }
+                      ],
+                      staticClass: "form-control",
+                      class: { "is-invalid": _vm.errors.password },
+                      attrs: { type: "password", placeholder: "Password" },
+                      domProps: { value: _vm.data.password },
+                      on: {
+                        keyup: _vm.clearError,
+                        input: function($event) {
+                          if ($event.target.composing) {
+                            return
+                          }
+                          _vm.$set(_vm.data, "password", $event.target.value)
+                        }
+                      }
+                    }),
+                    _vm._v(" "),
+                    _vm.errors.password
+                      ? _c("div", { staticClass: "invalid-feedback" }, [
+                          _vm._v(_vm._s(_vm.errors.password[0]) + " ")
+                        ])
+                      : _vm._e()
+                  ]),
+                  _vm._v(" "),
+                  _c(
+                    "b-button",
+                    {
+                      attrs: {
+                        variant: "dark",
+                        squared: "",
+                        disabled: _vm.isLoading
+                      },
+                      on: {
+                        click: function($event) {
+                          $event.preventDefault()
+                          return _vm.postLogin($event)
+                        }
+                      }
+                    },
+                    [
+                      _c("b-spinner", {
+                        directives: [
+                          {
+                            name: "show",
+                            rawName: "v-show",
+                            value: _vm.isLoading,
+                            expression: "isLoading"
+                          }
+                        ],
+                        attrs: { small: "", type: "grow" }
+                      }),
+                      _vm._v("\n            Login\n          ")
                     ],
-                    attrs: { small: "", type: "grow" }
-                  }),
-                  _vm._v("\n            Login\n          ")
+                    1
+                  )
                 ],
                 1
               )
-            ],
-            1
+            ]
           )
-        ])
-      ])
+        ]
+      )
     ])
   ])
 }
@@ -57097,9 +57123,18 @@ var staticRenderFns = [
     var _vm = this
     var _h = _vm.$createElement
     var _c = _vm._self._c || _h
-    return _c("div", { staticClass: "card-header" }, [
-      _c("h4", [_vm._v("Vlam-Srv Login")])
+    return _c("div", { staticClass: "col-md-6" }, [
+      _c("img", {
+        staticStyle: { width: "100%", opacity: "0.5" },
+        attrs: { src: "/img/brand/bg-front.svg" }
+      })
     ])
+  },
+  function() {
+    var _vm = this
+    var _h = _vm.$createElement
+    var _c = _vm._self._c || _h
+    return _c("div", [_c("h4", [_vm._v("Vlam-Srv Login")])])
   }
 ]
 render._withStripped = true
