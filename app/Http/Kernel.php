54788b5f4d540e4c81a6981993d5801dcc7128cb
Shellrean <reanyusufali@gmail.com>
2020-01-31 06:50:05 +0700

    Ip Checker

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -69,6 +69,7 @@ class Kernel extends HttpKernel
         'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
+        'ip'    => \App\Http\Middleware\IpMiddleware::class,
     ];
 
     /**
--- routes/web.php
+++ routes/web.php
@@ -11,4 +11,4 @@
 |
 */
 
-Route::view('/{any}', 'index')->where('any','.*');
+Route::view('/{any}', 'index')->where('any','.*')->middleware(['ip']);
